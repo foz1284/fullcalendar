d123d297dc2b7088ad747a90b411f7979e9b082a
Mark Forster <foz1284@gmail.com>
2012-09-11 10:07:57 +0200

    Update src/common/CoordinateGrid.js
    Handles Cell selection correctly if there is a scrollbar.
    
    I specifically needed this for Next Weeks view as I am displaying 80 days at a time which requires a scroll bar and it screws up the cell selection.

--- src/common/CoordinateGrid.js
+++ src/common/CoordinateGrid.js
@@ -1,4 +1,3 @@
-
 function CoordinateGrid(buildFunc) {
 
 	var t = this;
@@ -17,14 +16,22 @@ function CoordinateGrid(buildFunc) {
 		var rowCnt = rows.length;
 		var colCnt = cols.length;
 		var i, r=-1, c=-1;
+
+		var scrollTopOffset = 0;
+	    	var scrollLeftOffset = 0;
+		if ($(".fc-grid")[0]) {
+		    scrollTopOffset = $(".fc-grid").scrollTop();
+		    scrollLeftOffset = $(".fc-grid").scrollLeft();
+		}
+
 		for (i=0; i<rowCnt; i++) {
-			if (y >= rows[i][0] && y < rows[i][1]) {
+			if (y + scrollTopOffset >= rows[i][0] && y + scrollTopOffset < rows[i][1]) {
 				r = i;
 				break;
 			}
 		}
 		for (i=0; i<colCnt; i++) {
-			if (x >= cols[i][0] && x < cols[i][1]) {
+			if (x + scrollLeftOffset >= cols[i][0] && x + scrollLeftOffset < cols[i][1]) {
 				c = i;
 				break;
 			}
